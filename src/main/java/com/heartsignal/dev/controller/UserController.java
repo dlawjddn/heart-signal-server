c8cdf2815f2429e77988b208cca8c0a1d1f736eb
dlawjddn <jwl5015@naver.com>
2023-10-05 04:30:51 +0900

    Style: user controller와 userinfo controller 합침

--- src/main/java/com/heartsignal/dev/controller/UserController.java
+++ src/main/java/com/heartsignal/dev/controller/UserController.java
@@ -2,14 +2,14 @@ package com.heartsignal.dev.controller;
 
 import com.heartsignal.dev.Facade.AggregationFacade;
 import com.heartsignal.dev.domain.User;
+import com.heartsignal.dev.dto.userInfo.request.SaveAdditionalInfo;
 import com.heartsignal.dev.dto.userInfo.response.AdditionalInfoDTO;
+import com.heartsignal.dev.dto.userInfo.response.ExistedNickname;
 import com.heartsignal.dev.oauth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -17,6 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
     private final AggregationFacade aggregationFacade;
+    @PostMapping("/additional")
+    public void saveAdditionalInfo(@RequestBody SaveAdditionalInfo additionalInfo, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        User user = principalDetails.getUser();
+        aggregationFacade.saveAdditionalInfo(user, additionalInfo);
+    }
+    @GetMapping("/existed-nickname/{nickname}")
+    public ExistedNickname checkDuplicatedNickname(@PathVariable String nickname){
+        return aggregationFacade.checkDuplicatedNickname(nickname);
+    }
     @GetMapping("/mypage")
     public AdditionalInfoDTO showMyPage(@AuthenticationPrincipal PrincipalDetails principalDetails){
         User user = principalDetails.getUser();
