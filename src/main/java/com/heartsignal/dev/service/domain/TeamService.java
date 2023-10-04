968ddb9c9fbda4c152c16014c5e788fdd55b4d3b
dlawjddn <jwl5015@naver.com>
2023-10-05 03:23:19 +0900

    Feat: 팀 구성 및 저장 service 로직 생성

--- src/main/java/com/heartsignal/dev/service/domain/TeamService.java
+++ src/main/java/com/heartsignal/dev/service/domain/TeamService.java
@@ -1,13 +1,24 @@
 package com.heartsignal.dev.service.domain;
 
 
+import com.heartsignal.dev.domain.Team;
+import com.heartsignal.dev.domain.User;
 import com.heartsignal.dev.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TeamService {
 
     private final TeamRepository teamRepository;
+    @Transactional
+    public void saveTeam(User leader, List<User> members, String title){
+        teamRepository.save(new Team(leader, title, members));
+        log.info("팀 구성 완료");
+    }
 }
