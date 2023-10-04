20a9c73ba40d6d898b0b1981dfbeccb156126373
dlawjddn <jwl5015@naver.com>
2023-10-05 05:44:18 +0900

    Feat: bar service 생성

--- src/main/java/com/heartsignal/dev/service/domain/BarService.java
+++ src/main/java/com/heartsignal/dev/service/domain/BarService.java
@@ -1,13 +1,22 @@
 package com.heartsignal.dev.service.domain;
 
 
+import com.heartsignal.dev.domain.Bar;
 import com.heartsignal.dev.repository.BarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BarService {
-
     private final BarRepository barRepository;
+    public List<String> findLocations(){
+        return barRepository.findLocation();
+    }
+    public List<Bar> findBarsInLocation(String location){
+        return barRepository.findByLocation(location);
+    }
+
 }
