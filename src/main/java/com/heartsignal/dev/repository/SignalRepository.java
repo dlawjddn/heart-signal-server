e079533570551d436557797654c6a9a732e76e8c
dlawjddn <jwl5015@naver.com>
2023-10-05 06:18:02 +0900

    Feat: 보낸 사람, 받은 사람 동시 조회 로직 생성

--- src/main/java/com/heartsignal/dev/repository/SignalRepository.java
+++ src/main/java/com/heartsignal/dev/repository/SignalRepository.java
@@ -5,8 +5,10 @@ import com.heartsignal.dev.domain.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SignalRepository extends JpaRepository<Signal, Long> {
     List<Signal> findBySender(Team sendTeam);
     List<Signal> findByReceiver(Team receivedTeam);
+    Optional<Signal> findBySenderAndReceiver(Team sender, Team receiver);
 }
