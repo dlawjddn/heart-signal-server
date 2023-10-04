3cb064dd28255b8e6f95328c7075e90c49362a16
dlawjddn <jwl5015@naver.com>
2023-10-05 01:42:43 +0900

    Feat: 회원 추가 정보 save request dto 생성

---
+++ src/main/java/com/heartsignal/dev/dto/userInfo/SaveAdditionalInfo.java
@@ -0,0 +1,12 @@
+package com.heartsignal.dev.dto.userInfo;
+
+import lombok.Data;
+
+@Data
+public class SaveAdditionalInfo {
+    private String nickname;
+    private String mbti;
+    private String face;
+    private String selfInfo;
+    private String gender;
+}
