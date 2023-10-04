738e16f9586c80216f40260a91a2f42db108cf8d
dlawjddn <jwl5015@naver.com>
2023-10-05 05:54:52 +0900

    Feat: 내가 보낸, 내가 받은 시그널 조회 로직 생성

--- src/main/java/com/heartsignal/dev/repository/SignalRepository.java
+++ src/main/java/com/heartsignal/dev/repository/SignalRepository.java
@@ -1,7 +1,12 @@
 package com.heartsignal.dev.repository;
 
 import com.heartsignal.dev.domain.Signal;
+import com.heartsignal.dev.domain.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SignalRepository extends JpaRepository<Signal, Long> {
+    List<Signal> findBySender(Team sendTeam);
+    List<Signal> findByReceiver(Team receivedTeam);
 }
