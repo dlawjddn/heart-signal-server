7b3711e4a7ec51361f8e883b652c64b09d937926
dlawjddn <jwl5015@naver.com>
2023-10-05 01:43:46 +0900

    Feat: facade 적용 완료

--- src/main/java/com/heartsignal/dev/Facade/AggregationFacade.java
+++ src/main/java/com/heartsignal/dev/Facade/AggregationFacade.java
@@ -1,10 +1,13 @@
 package com.heartsignal.dev.Facade;
 
+import com.heartsignal.dev.domain.User;
 import com.heartsignal.dev.dto.userInfo.DuplicatedNickname;
+import com.heartsignal.dev.dto.userInfo.SaveAdditionalInfo;
 import com.heartsignal.dev.service.domain.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -16,6 +19,13 @@ public class AggregationFacade {
     private final MeetingRoomService meetingRoomService;
     private final BarService barService;
     private final BarChatroomService barChatroomService;
+
+    /**
+     * 추가 정보 기입
+     */
+    public void saveAdditionalInfo(User user, SaveAdditionalInfo additionalInfo){
+        userInfoService.saveAdditionalInfo(user, additionalInfo);
+    }
 
     public DuplicatedNickname checkDuplicatedNickname(String nickname){
         return new DuplicatedNickname(userInfoService.isExistedNickname(nickname));
