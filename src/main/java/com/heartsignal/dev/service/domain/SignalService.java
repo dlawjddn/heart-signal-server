f0eea8a6a1034c44ba9c4b1c875861d92b20ba18
dlawjddn <jwl5015@naver.com>
2023-10-05 05:55:11 +0900

    Feat: 내가 보낸, 내가 받은 시그널 service 로직 생성

--- src/main/java/com/heartsignal/dev/service/domain/SignalService.java
+++ src/main/java/com/heartsignal/dev/service/domain/SignalService.java
@@ -2,11 +2,14 @@ package com.heartsignal.dev.service.domain;
 
 import com.heartsignal.dev.domain.Signal;
 import com.heartsignal.dev.domain.Team;
+import com.heartsignal.dev.domain.User;
 import com.heartsignal.dev.repository.SignalRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -21,5 +24,11 @@ public class SignalService {
                         .receiver(receivedTeam)
                         .build());
         log.info("시그널 저장 성공");
+    }
+    public List<Signal> findSendingSignal(Team myTeam){
+        return signalRepository.findBySender(myTeam);
+    }
+    public List<Signal> findReceivedSignal(Team myTeam){
+        return signalRepository.findByReceiver(myTeam);
     }
 }
