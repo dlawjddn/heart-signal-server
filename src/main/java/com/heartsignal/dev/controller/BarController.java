65dcfb66d4e68427e688c76ac06cab949a77786e
dlawjddn <jwl5015@naver.com>
2023-10-06 02:26:16 +0900

    Feat: 주점 채팅방 목록 api 생성

--- src/main/java/com/heartsignal/dev/controller/BarController.java
+++ src/main/java/com/heartsignal/dev/controller/BarController.java
@@ -1,14 +1,23 @@
 package com.heartsignal.dev.controller;
 
+import com.heartsignal.dev.Facade.AggregationFacade;
+import com.heartsignal.dev.dto.bar.response.BarListDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/bars")
 public class BarController {
-
+    private final AggregationFacade aggregationFacade;
+    @GetMapping
+    public List<BarListDTO> showBars(){
+        return aggregationFacade.provideBarInfos();
+    }
 }
