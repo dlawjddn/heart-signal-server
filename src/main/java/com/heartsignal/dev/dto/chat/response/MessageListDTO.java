c5a15e812f58aaa1c4a20a8f87f3fcb677edcd72
jjuuuunnii <jake991110@naver.com>
2023-10-06 04:15:44 +0900

    Feat: 주점별 입장 api 추가

--- src/main/java/com/heartsignal/dev/dto/chat/response/MessageListDTO.java
+++ src/main/java/com/heartsignal/dev/dto/chat/response/MessageListDTO.java
@@ -1,7 +1,14 @@
 package com.heartsignal.dev.dto.chat.response;
 
+import lombok.*;
+
 import java.util.List;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageListDTO {
     private List<MessageDTO> messageList;
 }
