8ee50dc5bb1266f2bd871e26364c5d0350dfecd7
jjuuuunnii <jake991110@naver.com>
2023-10-06 01:39:08 +0900

    Fix: Message 변수 수정

--- src/main/java/com/heartsignal/dev/domain/nosql/Message.java
+++ src/main/java/com/heartsignal/dev/domain/nosql/Message.java
@@ -3,6 +3,8 @@ package com.heartsignal.dev.domain.nosql;
 
 import lombok.*;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -11,7 +13,7 @@ import lombok.*;
 public class Message {
     private String sender;
     private String content;
-    private String send_at;
+    private Date sendAt;
 
     /**
      * message는 따로 레포, 서비스를 만들지 않음!
