bd60a1ffbace8d09997582577fba12a4fb00f6ed
jjuuuunnii <jake991110@naver.com>
2023-10-06 23:24:05 +0900

    Feat: teamService,             entityManager.flush();

--- src/main/java/com/heartsignal/dev/Facade/AggregationFacade.java
+++ src/main/java/com/heartsignal/dev/Facade/AggregationFacade.java
@@ -157,9 +157,6 @@ public class AggregationFacade {
          * 만약 true면
          * 1. myTeam, OtherTeam의 status를 true로 업데이트 하기
          * 2. 채팅방 반들기
-         * 3.   myTeam.updateTeamStatus(true);
-         *             otherTeam.updateTeamStatus(true);
-         *             2개 뺴기
          * 4. 동겸이가 할일 시그널 다지우기
          */
         if (signalService.isMutualFollow(myTeam, otherTeam)) {
@@ -279,6 +276,7 @@ public class AggregationFacade {
                 .collect(Collectors.toList());
 
         return MessageListDTO.builder()
+                .id(chatId)
                 .messageList(messageDTOList)
                 .build();
     }
