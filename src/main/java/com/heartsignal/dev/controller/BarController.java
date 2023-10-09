c956312f5011e798e9f89a3c08426348571dae68
kurtyoon <akakertcobain@naver.com>
2023-10-10 03:27:33 +0900

    Feat: 신고 누적 유저 기능 금지

--- src/main/java/com/heartsignal/dev/controller/BarController.java
+++ src/main/java/com/heartsignal/dev/controller/BarController.java
@@ -1,11 +1,14 @@
 package com.heartsignal.dev.controller;
 
 import com.heartsignal.dev.Facade.AggregationFacade;
+import com.heartsignal.dev.domain.rds.User;
 import com.heartsignal.dev.dto.bar.response.BarListDTO;
 import com.heartsignal.dev.dto.chat.response.MessageListDTO;
+import com.heartsignal.dev.oauth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
@@ -18,8 +21,9 @@ import java.util.List;
 public class BarController {
     private final AggregationFacade aggregationFacade;
     @GetMapping
-    public List<BarListDTO> showBars(){
-        return aggregationFacade.provideBarInfos();
+    public List<BarListDTO> showBars(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        User user = principalDetails.getUser();
+        return aggregationFacade.provideBarInfos(user);
     }
 
 
